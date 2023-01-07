12079db8c4e65bc92e9c668e789df9ccac82bd9a
Sampath <igisampa@gmail.com>
2023-01-08 01:52:20 +0530

    upadate employeee save and view

--- src/component/Employee/employee_attendence.tsx
+++ src/component/Employee/employee_attendence.tsx
@@ -1,10 +1,19 @@
-import React ,{ChangeEvent, useState }  from 'react';
+import React ,{ChangeEvent, useEffect, useState }  from 'react';
+import { Card } from '@mui/material';
+
 import Auth from '../../services/api';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
 const EmployeeAttend = () =>{
+  
 
     // const {http,setToken} = Auth();
     const [file,setFile] = useState<File>();
-
+    const [employee,setEmployee] = useState([]);
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             
@@ -21,17 +30,64 @@ const EmployeeAttend = () =>{
           "Content-Type": "multipart/form-data",
         }
     }).then((res)=>{
+    console.log(res);
            
-        })
+    });
     }
 
     }
+    const getData =()=>{
+      
+      Auth.get('/getsheduledata').then((res)=>{
+        setEmployee(res.data);
+
+
+      });  
+  }
+    useEffect(() => {getData()}, []);
+    
     
     return (
-        <div>
-            <input type="file" name='exel' onChange={handleFileChange}/>
-            <button onClick={()=>onSubmit()}>Submit</button>
-        </div>
+      <>
+          <Card style={{marginTop:'2%',marginLeft:'2%'}}>
+            <div>
+                <input type="file" name='exel' onChange={handleFileChange}/>
+                <button onClick={()=>onSubmit()}>Submit</button>
+            </div>
+          </Card>
+          <Card style={{marginTop:'2%',marginLeft:'2%'}}>
+              <TableContainer>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Name</TableCell>
+                      <TableCell>checkin</TableCell>
+                      <TableCell>checkout</TableCell>
+                      <TableCell>Working hours</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    
+                    {employee.map((row) => (
+                      <TableRow
+                        key={row.sch_id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell component="th" scope="row">
+                          {row.emp_nam}
+                        </TableCell>
+                        <TableCell>{row.attend == 1?"Yes":"No"}</TableCell>
+                        <TableCell >{row.attend == 1?"No":"Yes"}</TableCell>
+                        <TableCell>{row.time}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+              </Table>
+    </TableContainer>
+          </Card>
+
+        </>
+        
     );
 }
 export default EmployeeAttend;
